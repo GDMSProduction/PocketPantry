9204c304c62adb0330b94a71eb4c66880a51abaf
Justin Smith <JPSmith@student.fullsail.edu>
2018-06-13 21:26:32 -0400

    Navigation drawer now works on all screens except EntryUI

--- GroceryAppV1/app/src/main/java/com/softwaredev/groceryappv1/RecipeList.java
+++ GroceryAppV1/app/src/main/java/com/softwaredev/groceryappv1/RecipeList.java
@@ -5,8 +5,14 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.transition.Fade;
 import android.view.ContextMenu;
 import android.view.MenuInflater;
@@ -28,12 +34,55 @@ public class RecipeList extends AppCompatActivity {
     static String RecName;
     static String RecipeName;
     static SharedPreferences recSharedPref;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_list);
         recSharedPref = this.getSharedPreferences("com.softwaredev.groceryappv1.recipeList", Context.MODE_PRIVATE);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+        actionbar.setTitle("Recipe List");
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+
+                        if (menuItem.toString().equals("Pantry"))
+                        {
+                            sendPantry();
+                        }
+                        else if (menuItem.toString().equals("Grocery List"))
+                        {
+                            sendGroc();
+                        }
+                        else if (menuItem.toString().equals("About"))
+                        {
+                            sendHelp();
+                        }
+
+                        return true;
+                    }
+                }
+        );
 
 
         int listSize = recSharedPref.getInt("size", 0);
@@ -82,6 +131,16 @@ public class RecipeList extends AppCompatActivity {
         });
 
         registerForContextMenu(listView);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -170,6 +229,34 @@ public class RecipeList extends AppCompatActivity {
             }
         }
         Populate();
+    }
+
+    public void sendPantry()
+    {
+
+        Intent pantryIntent = new Intent(this, PantryUI.class);
+        pantryIntent.putExtra("isPantry", true);
+        startActivity(pantryIntent);
+
+    }
+    public void sendGroc()
+    {
+        Intent grocIntent = new Intent(this, PantryUI.class);
+        grocIntent.putExtra("isPantry", false);
+        startActivity(grocIntent);
+
+    }
+    public void sendRecipe()
+    {
+        Intent recipeIntent = new Intent(this, RecipeList.class);
+        startActivity(recipeIntent);
+
+    }
+    public void sendHelp()
+    {
+        Intent helpIntent = new Intent(this, HelpUI.class);
+        startActivity(helpIntent);
+
     }
 
     public static void addToRecList(RecipeBase recipe)
