407f9380fb675b941e0e0b0c75b17cf94a3950e2
DeAngelo Rieke <DERieke@student.fullsail.edu>
2018-06-21 17:00:15 -0400

    RecipeTut.java added to run the user through an outlined recipe tutorial.

--- GroceryAppV1/app/src/main/java/com/softwaredev/groceryappv1/RecipeExp.java
+++ GroceryAppV1/app/src/main/java/com/softwaredev/groceryappv1/RecipeExp.java
@@ -3,12 +3,14 @@ package com.softwaredev.groceryappv1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class RecipeExp extends AppCompatActivity {
 
     static String RecipeText;
     int position;
+    boolean complete = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,18 +23,22 @@ public class RecipeExp extends AppCompatActivity {
         DisplayRec(RecipeList.GetName());
         if(RecipeList.GetName().equals("Grilled Cheese"))
         {
-            RecipeText = "Place bread butter-side-down onto skillet bottom and add 1 slice of cheese. Butter a second slice of bread on one side and place butter-side-up on top of sandwich. Grill until lightly browned and flip over; continue grilling until cheese is melted. Repeat with remaining 2 slices of bread, butter and slice of cheese.";
+            RecipeText = "A golden and melted blend on a traditional grilled cheese";
+                    // "Place bread butter-side-down onto skillet bottom and add 1 slice of cheese. Butter a second slice of bread on one side and place butter-side-up on top of sandwich. Grill until lightly browned and flip over; continue grilling until cheese is melted. Repeat with remaining 2 slices of bread, butter and slice of cheese.";
             DisplayIns(RecipeText);
+            findViewById(R.id.StartButton).setVisibility(View.VISIBLE);
         }
         else if(RecipeList.GetName().equals("Lemon Pepper Chicken"))
         {
             RecipeText = "Preheat oven to 350 degrees F (175 degrees C).\n" +"Place chicken in a lightly greased 9x13 inch baking dish. Season with lemon pepper, garlic powder and onion powder to taste. Bake in preheated oven for 15 minutes.\n" + "Turn over chicken pieces and add more seasoning to taste. Bake for an additional 15 minutes, or until chicken is cooked through and juices run clear.";
             DisplayIns(RecipeText);
+            findViewById(R.id.StartButton).setVisibility(View.INVISIBLE);
         }
         else if(RecipeList.GetName().equals("Garlic Rosemary Lamb"))
         {
             RecipeText = "This would be way to difficult to type, we can get back to this when the tutorial system is done.";
             DisplayIns(RecipeText);
+            findViewById(R.id.StartButton).setVisibility(View.INVISIBLE);
         }
         else
         {
@@ -53,4 +59,10 @@ public class RecipeExp extends AppCompatActivity {
             TextView insText = findViewById(R.id.Instructions);
             insText.setText(Ins);
     }
+    public void StartRec(View v)
+    {
+        Intent recipeIntent = new Intent(this, RecipeTut.class);
+        startActivity(recipeIntent);
+    }
+
 }
---
+++ GroceryAppV1/app/src/main/java/com/softwaredev/groceryappv1/RecipeTut.java
@@ -0,0 +1,54 @@
+package com.softwaredev.groceryappv1;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class RecipeTut extends AppCompatActivity {
+    int Step = 2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_tut);
+        final Button button = findViewById(R.id.nextStepButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Step == 2)
+                {
+                    TextView textView = findViewById(R.id.insTextView);
+                    textView.setText("Use your cheese slicer to cut generous slices off your block of white cheddar\nSet these to the side for now, grab two slices of break and butter one side generously on each\nSet your skillet over low-medium heat and move onto a next step where a timer will start!");
+                    Step ++;
+                }
+                else if(Step == 3)
+                {
+                    final TextView textView = findViewById(R.id.insTextView);
+                    new CountDownTimer(5000, 1000) {
+
+                        public void onTick(long millisUntilFinished) {
+                            textView.setText("seconds remaining: " + millisUntilFinished / 1000);
+                        }
+
+                        public void onFinish() {
+                            textView.setText("Now place the bread butter side down into the skillet placing 2 slices of cheddar on one piece of bread and 2 slices of munster on the other\nwait until you can see the cheese start to glisten and melt. Use your flipper to place the two pieces of bread together\nContinue cooking each side evenly flipping the sandwich frequently until golden brown\n Slice and enjoy!");
+                            Step++;
+                        }
+                    }.start();
+                }
+                else if(Step == 4)
+                {
+                    TextView textView = findViewById(R.id.insTextView);
+                    textView.setText("Congratulations, you actually clicked through this mess of an example.");
+                }
+                else
+                {
+                    Step = 4;
+                }
+            }
+        });
+    }
+}
