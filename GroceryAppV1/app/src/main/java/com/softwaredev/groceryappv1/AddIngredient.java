eb09ac6a0d6d9718fc6dc5f954e5e095c4ad6743
Justin Smith <JPSmith@student.fullsail.edu>
2018-06-21 17:21:22 -0400

    Ingredients added to recipes

--- GroceryAppV1/app/src/main/java/com/softwaredev/groceryappv1/AddIngredient.java
+++ GroceryAppV1/app/src/main/java/com/softwaredev/groceryappv1/AddIngredient.java
@@ -6,17 +6,49 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class AddIngredient extends AppCompatActivity {
+
+    String mName;
+    String mUnit;
+    int mQuantity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_ingredient);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
 
+    }
+
+    public void addButtonPressed(View view)
+    {
+
+        EditText NameEditText = findViewById(R.id.nameInput);
+        EditText QuantEditText = findViewById(R.id.quantityInput);
+        EditText UnitEditText = findViewById(R.id.unitInput);
+
+        if (NameEditText.getText().toString().trim().length() <= 0)
+            return;
+
+        mName = NameEditText.getText().toString();
+
+        if (QuantEditText.getText().toString().length() >= 10)
+            mQuantity = 999999999;
+        else if (QuantEditText.getText().toString().trim().length() <= 0)
+            mQuantity = 1;
+        else
+            mQuantity = Integer.valueOf(QuantEditText.getText().toString());
+
+        if (UnitEditText.getText().toString().trim().length() <= 0)
+            mUnit = "";
+        else
+            mUnit = UnitEditText.getText().toString();
+
+        AddRecipe.addIngredient(new Item(mName, mQuantity, mUnit));
+
+        finish();
     }
 
 }
--- GroceryAppV1/app/src/main/java/com/softwaredev/groceryappv1/AddRecipe.java
+++ GroceryAppV1/app/src/main/java/com/softwaredev/groceryappv1/AddRecipe.java
@@ -5,26 +5,42 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class AddRecipe extends AppCompatActivity {
+
+    String recipeName;
+    String recipeInstructions;
+    private static ArrayList<Item> ingredients = new ArrayList<>(1);
+    private static ArrayList<String> ingredientsString = new ArrayList<> (1);
+    ArrayAdapter<String> ingAdapt;
+    ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_recipe);
+
+        ingAdapt = new ArrayAdapter(this, android.R.layout.simple_list_item_1, ingredientsString);
+        listView = findViewById(R.id.ingredientsListView);
+        listView.setAdapter(ingAdapt);
+
         FloatingActionButton fab = findViewById(R.id.addRecipeFAB);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 EditText nameEditText = findViewById(R.id.editRecNameText);
-                String recipeName;
+
                 if (nameEditText.getText().toString().trim().length() <= 0)
                     return;
                 else
                     recipeName = nameEditText.getText().toString();
 
                 EditText insEditText = findViewById(R.id.editRecDescText);
-                String recipeInstructions;
                 if (insEditText.getText().toString().trim().length() <= 0)
                     return;
                 else
@@ -35,4 +51,25 @@ public class AddRecipe extends AppCompatActivity {
             }
         });
 }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        ingAdapt = new ArrayAdapter(this, android.R.layout.simple_list_item_1, ingredientsString);
+        listView = findViewById(R.id.ingredientsListView);
+        listView.setAdapter(ingAdapt);
+    }
+
+    public void sendAddIngredient(View view)
+    {
+        Intent ingredientIntent = new Intent(this, AddIngredient.class);
+        startActivity(ingredientIntent);
+    }
+
+    public static void addIngredient (Item ingredient)
+    {
+        ingredients.add(ingredient);
+        ingredientsString.add(ingredient.ingredientToString());
+    }
 }
